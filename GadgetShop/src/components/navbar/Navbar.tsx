10d13dffdef6bedea6240b46e18b332797e86085
behzadk2 <bkazemi1994@gmail.com>
2024-06-12 18:21:53 +0200

    style : change navbar style

--- GadgetShop/src/components/navbar/Navbar.tsx
+++ GadgetShop/src/components/navbar/Navbar.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className="h-8 border-b shadow">
+    <div className="h-8 border-b shadow flex justify-between flex-row items-center">
+      <ul className='flex'>
+        <li><Link to='{/}' >Home</Link></li>
+        <li><Link to='{/}' >Store</Link></li>
+
+      </ul>
+      <div>
+        <button>basket</button>
+      </div>
       
     </div>
   )
