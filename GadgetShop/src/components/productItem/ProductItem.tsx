cc868ed9580ab6565f7e09b8f4f8161ed92e788a
behzadk2 <bkazemi1994@gmail.com>
2024-06-13 15:17:09 +0200

    style : add ProductItem

---
+++ GadgetShop/src/components/productItem/ProductItem.tsx
@@ -0,0 +1,20 @@
+
+
+function ProductItem() {
+  return (
+    <div className="shadow border rounded pb-2">
+      <img className="rounded-t" src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+      <div className="flex justify-between flex-row px-4 mt-2">
+        <h3>Product Title </h3>
+        <span>55 $</span>
+      </div>
+      <div className="px-4 mt-1">
+       <p className="line-clamp-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, minus velit aut amet similique quae blanditiis. Porro blanditiis ducimus doloremque fugit iure deserunt quisquam aspernatur obcaecati quaerat perferendis, in est?
+       </p> 
+       </div>
+    </div>
+  )
+}
+
+export default ProductItem
--- GadgetShop/src/pages/store/Store.tsx
+++ GadgetShop/src/pages/store/Store.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import ProductItem from '../../components/productItem/ProductItem'
+import Container from '../../components/container/Container'
 
 function Store() {
   return (
     <div>
-      Store
+      <Container>
+      <h1 className='mt-5 '>New Product</h1>
+      <div className='grid grid-cols-4 gap-4 mt-4'>
+      <ProductItem/>
+      <ProductItem/>
+      <ProductItem/>
+      <ProductItem/>
+      </div>
+      </Container>
+     
     </div>
   )
 }
