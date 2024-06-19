a5366f550c451014a0e81e57c152fad690b7ff9f
behzadk2 <bkazemi1994@gmail.com>
2024-06-19 18:50:56 +0200

    style : add style to prudctItem

--- GadgetShop/src/components/productItem/ProductItem.tsx
+++ GadgetShop/src/components/productItem/ProductItem.tsx
@@ -1,22 +1,23 @@
-import { Product } from "../../types/server";
+import React from 'react';
+import { Product } from '../../types/server';
 
-
-
-function ProductItem({title,price,image,description}: Product) {
-   return (
-    <div className="shadow border rounded pb-2">
-      <img className="rounded-t size-40" src={image} alt="" />
-      <div className="flex justify-between flex-row px-4 mt-2">
-        <h3>{title}</h3>
-        <span>{price} $</span>
+const ProductItem = ({ title, price, image, description }: Product) => {
+  return (
+    <div className="shadow-lg border rounded-lg flex flex-col hover:shadow-xl transition-shadow duration-300 h-full">
+      <div className="overflow-hidden rounded-t-lg h-56">
+        <img className="w-full h-full object-contain" src={image} alt={title} />
       </div>
-      <div className="px-4 mt-1">
-       <p className="line-clamp-5 text-gray-400 ">
-        Description : {description}
-        </p> 
-       </div>
+      <div className="p-4 flex flex-col flex-grow">
+        <div className="flex justify-between items-center mb-2">
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <span className="text-xl font-bold text-blue-600">${price}</span>
+        </div>
+        <p className="text-gray-600 text-sm flex-grow">
+          Description: {description}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductItem
+export default ProductItem;
--- GadgetShop/src/pages/store/Store.tsx
+++ GadgetShop/src/pages/store/Store.tsx
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import ProductItem from '../../components/productItem/ProductItem'
-import Container from '../../components/container/Container'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import ProductItem from '../../components/productItem/ProductItem';
+import Container from '../../components/container/Container';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Product } from '../../types/server';
 
-
 function Store() {
-  const[product,setProduct] = useState<Product[]>([]);
-  useEffect(()=>{
-    const getData = async() =>{
-      const response = await axios.get('https://fakestoreapi.com/products');
-      setProduct(response.data);
-    }
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
     getData();
-    
-  },[])
-  console.log(product);
+  }, []);
+
   return (
- 
     <div>
       <Container>
-      <h1 className='mt-5 '>New Product</h1>
-      <div className='grid grid-cols-4 gap-4 mt-4'>
-        {product.map((item) => ( <Link to={`/product/${1}`} ><ProductItem {...item} /></Link>))}
-     
-      
-      </div>
+        <h1 className="mt-5 text-3xl font-bold text-center">New Products</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-8">
+          {products.map(item => (
+            <Link to={`/product/${item.id}`} key={item.id}>
+              <ProductItem {...item} />
+            </Link>
+          ))}
+        </div>
       </Container>
-     
     </div>
-  )
+  );
 }
 
-export default Store
+export default Store;
